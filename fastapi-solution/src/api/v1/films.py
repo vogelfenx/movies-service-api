67a4c7ab3ed0ef670952f895e7b22bd1d4c726d2
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-04-24 01:47:30 +0200

    Extend films/id endpoint
    - Use models.Film model to set fields to be returned as specified in the API endpoint schema.

--- fastapi-solution/src/api/v1/films.py
+++ fastapi-solution/src/api/v1/films.py
@@ -1,8 +1,8 @@
 from http import HTTPStatus
 
 from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
 
+from models import Film
 from services.film import FilmService, get_film_service
 
 router = APIRouter()
@@ -23,7 +23,8 @@ async def film_details(
         # Если фильм не найден, отдаём 404 статус
         # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum
         # Такой код будет более поддерживаемым
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
+                            detail="film not found")
 
     # Перекладываем данные из models.Film в Film
     # Обратите внимание, что у модели бизнес-логики есть поле description
@@ -31,4 +32,13 @@ async def film_details(
     # Если бы использовалась общая модель для бизнес-логики и формирования ответов API
     # вы бы предоставляли клиентам данные, которые им не нужны
     # и, возможно, данные, которые опасно возвращать
-    return Film(id=film.id, title=film.title)
+    return Film(
+        id=film.id,
+        title=film.title,
+        imdb_rating=film.imdb_rating,
+        description=film.description,
+        genre=film.genre,
+        actors=film.actors,
+        writers=film.writers,
+        director=film.director,
+    )
