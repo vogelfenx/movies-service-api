05daff2cf8c7d66cd93c99d13d611958c54ef38a
Evgeny V. Astakhov <Astakhov_E@avp.ru>
2023-04-23 23:21:25 +0300

    New structure

--- fastapi-solution/Docker_settings/etl/commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /etl/wait_services_up.sh
-sleep 5 #нужно, чтобы успел выполниться скрипт по созданию схемы в БД
-cd /etl/sqlite_to_postgres && python3 /etl/sqlite_to_postgres/load_data.py
-#идем обратно и запускаем скрипт ETL
-cd /etl && python3 /etl/etl_main.py
--- fastapi-solution/src/main.py
+++ fastapi-solution/src/main.py
@@ -42,5 +42,5 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8008,
+        port=8000,
     )
