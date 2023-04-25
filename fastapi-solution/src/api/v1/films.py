dfe88cf22cf2996e433b46f5b89a915d19160b42
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-04-25 17:45:47 +0200

    Implement API Endpoint to retrieve list of films
    - Retrieve films using pagination parameters page_size & page_number
    - Paginate elasicsearch request, if needed

--- fastapi-solution/src/api/v1/films.py
+++ fastapi-solution/src/api/v1/films.py
@@ -1,39 +1,91 @@
 from http import HTTPStatus
+from typing import Optional
 
 from fastapi import APIRouter, Depends, HTTPException
 
+from core import config
 from models import Film
 from services.film import FilmService, get_film_service
 
 router = APIRouter()
 
 
-@router.get("/", response_model=list[Film])
-async def films_list() -> list[Film]:
-    films = [Film(id='testid', title='some title'),
-             Film(id='testid2', title='some title2')]
-    return films
+@router.get("/", response_model=dict, response_model_exclude_unset=True)
+async def films_list(
+    page_size: Optional[int] = config.DEFAULT_ELASTIC_QUERY_SIZE,
+    page_number: Optional[int] = 1,
+    film_service: FilmService = Depends(get_film_service),
+) -> list[Film]:
+    """
+    Retrieve list of films.
+
+    The query parameters page_size and page_number can be used to paginate the result.
+
+    Args:
+        page_size (Optional[int]): The size of the films retrieved per page. Defaults to DEFAULT_ELASTIC_QUERY_SIZE.
+        page_number (Optional[int]): The page number to retrieve. Default is 1.
+        film_service (FilmService): Film service to retrieve films from the database.
+
+    Raises:
+        HTTPException: If requested page not found (HTTPStatus.NOT_FOUND).
+        HTTPException: If films cannot be found (HTTPStatus.NOT_FOUND).
+
+    Returns:
+        list[Film]: List of films.
+    """
+    if not page_number > 0:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
+                            detail="page not found")
+
+    films_count, films = await film_service.get_films_list(page_size, page_number)
+    films = (
+        Film(
+            id=film.id,
+            title=film.title,
+            imdb_rating=film.imdb_rating,
+        ) for film in films
+    )
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
+                            detail="films not found")
+
+    total_pages = films_count // page_size
+    total_pages += 1 if films_count % page_size > 0 else 0
+
+    next_page = page_number + 1 if page_number < total_pages else None
+    prev_page = page_number - 1 if page_number > 1 else None
+
+    return {
+        'films_count': films_count,
+        'total_pages': total_pages,
+        'next_page': next_page,
+        'prev_page': prev_page,
+        'films': films,
+    }
 
 
-# Внедряем FilmService с помощью Depends(get_film_service)
-@router.get("/{film_id}", response_model=Film)
+@router.get("/{film_id}", response_model=Film, response_model_exclude_unset=True)
 async def film_details(
     film_id: str, film_service: FilmService = Depends(get_film_service)
 ) -> Film:
+    """
+    Retrieve the details of a specific film.
+
+    Args:
+        film_id (str): The ID of the film to retrieve.
+        film_service (FilmService): Film service to retrieve films from the database.
+
+    Raises:
+        HTTPException: If the film with the specified ID cannot be found.
+
+    Returns:
+        Film: The film with the details.
+    """
     film = await film_service.get_by_id(film_id)
     if not film:
-        # Если фильм не найден, отдаём 404 статус
-        # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum
-        # Такой код будет более поддерживаемым
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
                             detail="film not found")
 
-    # Перекладываем данные из models.Film в Film
-    # Обратите внимание, что у модели бизнес-логики есть поле description
-    # Которое отсутствует в модели ответа API.
-    # Если бы использовалась общая модель для бизнес-логики и формирования ответов API
-    # вы бы предоставляли клиентам данные, которые им не нужны
-    # и, возможно, данные, которые опасно возвращать
     return Film(
         id=film.id,
         title=film.title,
--- fastapi-solution/src/services/film.py
+++ fastapi-solution/src/services/film.py
@@ -5,9 +5,12 @@ from elasticsearch import AsyncElasticsearch, NotFoundError
 from fastapi import Depends
 from redis.asyncio import Redis
 
+from core import config
+from core.logger import LOGGING
 from db.elastic import get_elastic
 from db.redis import get_redis
 from models.film import Film
+from typing import Optional, Tuple, Iterator
 
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5  # 5 минут
 
@@ -17,27 +20,129 @@ class FilmService:
         self.redis = redis
         self.elastic = elastic
 
-    # get_by_id возвращает объект фильма. Он опционален, так как фильм может отсутствовать в базе
+    async def get_films_list(
+        self,
+        page_size: Optional[int],
+        page_number: Optional[int]
+    ) -> Tuple[int, Iterator[Film]]:
+        """Fetch films.
+
+        Args:
+            page_size (Optional[int]): The size of the films retrieved per page.
+            page_number (Optional[int]): The page number to retrieve.
+
+        Returns:
+            Tuple[int, Iterator[Film]]: Total number of films and list of fetched films.
+        """
+        films_count, films = await self._films_list_from_cache()  # TODO redis
+
+        if page_number:
+            from_index = page_size * (page_number - 1)
+
+        if not films:
+            films_count, films = await self._get_films_list_from_elastic(
+                query_size=page_size,
+                from_index=from_index
+            )
+
+        # await self._put_films_to_cache(films) # TODO redis
+
+        return films_count, films
+
     async def get_by_id(self, film_id: str) -> Optional[Film]:
+        """Retrieve a film by ID.
+
+        Args:
+            film_id (str): The ID of the film to retrieve.
+
+        Returns:
+            Optional[Film]: The retrieved film.
+        """
         # Пытаемся получить данные из кеша, потому что оно работает быстрее
         film = await self._film_from_cache(film_id)
         if not film:
-            # Если фильма нет в кеше, то ищем его в Elasticsearch
             film = await self._get_film_from_elastic(film_id)
             if not film:
-                # Если он отсутствует в Elasticsearch, значит, фильма вообще нет в базе
                 return None
             # Сохраняем фильм  в кеш
             await self._put_film_to_cache(film)
 
         return film
 
+    async def _get_films_list_from_elastic(
+        self,
+        query_size: Optional[int],
+        from_index: Optional[int]
+    ) -> Tuple[int, Iterator[Film]]:
+        """Fetch films from elasticsearch.
+
+        If the requested query size is larger than the maximum query size (MAX_ELASTIC_QUERY_SIZE),
+        the query will be paginated using elasticsearch's scrolling functionality,
+        to avoid overloading elasticsearch.
+
+        Args:
+            query_size (Optional [int]): The size of the query to retrieve.
+            from_index (Optional[int]): The document number to return from.
+
+        Returns:
+            Tuple[int, Iterator[Film]]: Total number of documents in the index
+                                        and list of fetched films.
+        """
+        max_query_size = config.MAX_ELASTIC_QUERY_SIZE
+        paginate_query_request = False
+
+        if query_size > max_query_size:
+            paginate_query_request = True
+            query_size = max_query_size
+
+        search_query = {
+            'query': {
+                'match_all': {},
+            },
+            'sort': [{'imdb_rating': {'order': 'desc'}}],
+            'size': query_size,
+        }
+
+        if from_index >= 0:
+            search_query['from'] = from_index
+
+        scroll = None
+        if paginate_query_request:
+            scroll = '5m'
+
+        response = await self.elastic.search(index='movies', body=search_query, scroll=scroll)
+
+        films_count = response['hits']['total']['value']
+        hits = response['hits']['hits']
+
+        if paginate_query_request:
+            scroll_id = response['_scroll_id']
+            films = []
+            while hits:
+                films.extend([Film(**hit['_source']) for hit in hits])
+
+                response = await self.elastic.scroll(scroll_id=scroll_id, scroll=scroll)
+                scroll_id = response['_scroll_id']
+                hits = response['hits']['hits']
+        else:
+            films = (Film(**hit['_source']) for hit in hits)
+
+        return (films_count, films)
+
     async def _get_film_from_elastic(self, film_id: str) -> Optional[Film]:
+        """Fetch a film from elasticsearch by ID.
+
+        Args:
+            film_id (str): The ID of the film to retrieve.
+
+        Returns:
+            Optional[Film]: The retrieved film.
+        """
         try:
-            doc = await self.elastic.get(index="movies", id=film_id)
+            doc = await self.elastic.get(index='movies', id=film_id)
         except NotFoundError:
             return None
-        return Film(**doc["_source"])
+        return Film(**doc['_source'])
 
     async def _film_from_cache(self, film_id: str) -> Optional[Film]:
         # Пытаемся получить данные о фильме из кеша, используя команду get
@@ -49,6 +154,10 @@ class FilmService:
         # pydantic предоставляет удобное API для создания объекта моделей из json
         film = Film.parse_raw(data)
         return film
+
+    async def _films_list_from_cache(self) -> Optional[Film]:
+        # TODO redis: Redis Implementation
+        return None, None
 
     async def _put_film_to_cache(self, film: Film):
         # Сохраняем данные о фильме, используя команду set
