2dc4aa6c08835e7b064ac85b00bfe3e59fa1db14
Igor Shalyutin <IShalyutin@yandex.ru>
2023-05-04 17:11:45 +0300

    Igor: скорректировал функцию формирования ключа кеширования, а также влияние на сервисы фильмов и персоналий

--- fastapi-solution/src/services/film.py
+++ fastapi-solution/src/services/film.py
@@ -216,7 +216,7 @@ class FilmService:
         return film
 
     async def _films_list_from_cache(
-        self, args_key: list[str]
+        self, args_key: str
     ) -> tuple[int | None, list[Film] | None]:
         """
         Fetch films from cache
