0e8781b4ff0a2fece0291e3842b93657a9520784
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-05-21 01:15:28 +0200

    Add test_retrieve_genres_from_cache

--- fastapi-solution/tests/functional/src/genres_test.py
+++ fastapi-solution/tests/functional/src/genres_test.py
@@ -43,3 +43,47 @@ async def test_retrieve_genres_without_cache(
     assert response_status == 200
     assert len(genres) == len(response_body)
     assert expected_genres_ids == resp_genres_ids
+
+
+@pytest.mark.asyncio
+async def test_retrieve_genres_from_cache(
+    main_api_url,
+    make_get_request,
+    create_es_index,
+    es_clear_index,
+    es_write_data,
+):
+    await create_es_index(
+        index=genres_settings.es_index,
+        index_settings=genres_settings.es_index_genres_mapping["settings"],
+        index_mappings=genres_settings.es_index_genres_mapping["mappings"],
+    )
+
+    genres = generate_genres()
+    expected_genres_ids = set([genre['id'] for genre in genres])
+
+    await es_write_data(
+        genres,
+        genres_settings.es_index,
+        genres_settings.es_id_field,
+    )
+
+    api_endpoint_url = f"{main_api_url}/{genres_settings.api_endpoint_url}"
+
+    response_body, _, response_status = await make_get_request(
+        request_path=api_endpoint_url,
+        query_payload=None,
+    )
+
+    await es_clear_index(index=genres_settings.es_index)
+
+    response_body, _, response_status = await make_get_request(
+        request_path=api_endpoint_url,
+        query_payload=None,
+    )
+
+    resp_genres_ids = set([genre['uuid'] for genre in response_body])
+
+    assert response_status == 200
+    assert len(genres) == len(response_body)
+    assert expected_genres_ids == resp_genres_ids
