b158843a81226e591fe2edce5af6896394b636a7
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-05-24 19:33:31 +0200

    Add backoff to elasticsearch waiter

--- fastapi-solution/tests/functional/utils/wait_for_es.py
+++ fastapi-solution/tests/functional/utils/wait_for_es.py
@@ -1,20 +1,40 @@
-import time
+import asyncio
 from os import getenv
 
-from elasticsearch import Elasticsearch
+from aioretry import retry
+from elasticsearch import AsyncElasticsearch
+from utils.backoff_policy import retry_policy
+from utils.logger import get_logger
 
-es_host = getenv("ELASTIC_HOST", default="localhost")
-es_port = getenv("ELASTIC_PORT", default=9200)
+logger = get_logger(__name__)
 
-if __name__ == "__main__":
-    es_client = Elasticsearch(
+
+@retry(retry_policy)
+async def wait_for_es(es_client: AsyncElasticsearch):
+    """Check elasticsearch is ready to accept connections."""
+    logger.info("Ping elasticsearch")
+
+    if await es_client.ping():
+        logger.info("Elasticsearch is ready to accept connections")
+    else:
+        raise Exception
+
+    await es_client.close()
+
+
+async def main():
+    es_host = getenv("ELASTIC_HOST", default="localhost")
+    es_port = getenv("ELASTIC_PORT", default=9200)
+
+    es_client = AsyncElasticsearch(
         hosts=f"http://{es_host}:{es_port}",
         verify_certs=False,
     )
 
-    print(f"try elastic on {es_host}:{es_port}")
-    while True:
-        print("ping elasticsearch")
-        if es_client.ping():
-            break
-        time.sleep(1)
+    try:
+        await wait_for_es(es_client)
+    finally:
+        await es_client.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
